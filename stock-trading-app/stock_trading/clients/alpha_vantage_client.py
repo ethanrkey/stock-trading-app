56fadbfeb6456d55f03e32e10d873b17b18dfd0c
Ethan Key <ethanrkey23@gmail.com>
2024-12-10 18:18:07 -0500

    changed kitchen model to stock model in alpha client

--- stock-trading-app/stock_trading/clients/alpha_vantage_client.py
+++ stock-trading-app/stock_trading/clients/alpha_vantage_client.py
@@ -3,7 +3,7 @@ import os
 from typing import Optional, Dict, Any 
 import logging 
 
-from stock_trading.models.kitchen_model import Stock
+from stock_trading.models.stock_model import Stock
 from stock_trading.utils.logger import configure_logger
 
 logger = logging.getLogger(__name__)
